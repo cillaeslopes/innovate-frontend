c847d6dac58beff4a0d067c331d227fc59ecc71c
Pri Lopes <prilopes@Pris-MacBook-Pro.local>
2020-06-20 20:20:42 -0300

    title to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
+import Head from 'next/head'
 import MenuButton from '../components/MenuButton'
 import { Container, Header, Footer } from '../partials/Home/styled'
 import Main from '../partials/Home/Main'
 import Layout from '../components/Layout'
 
-type Props = {
-    name: string
-}
-
-const Home: React.FunctionComponent<Props> = ({ name }) => (
+const Home: React.FunctionComponent = () => (
     <Layout>
+        <Head>
+            <title>Innovate</title>
+        </Head>
         <Container>
             <Header>
                 <MenuButton />

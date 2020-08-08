215b06ccce1413bd8b1953145e61a46a41f28285
Fernanda Duarte <fernanda.aduarte@hotmail.com>
2020-08-08 18:49:30 -0300

    Change Home desktop container width

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { StylesProvider } from '@material-ui/core/styles'
+import {MenuContextProvider} from '../contexts/menuContext'
 
 type Props = {
     Component: React.FunctionComponent
@@ -9,7 +10,9 @@ type Props = {
 const MyApp: React.FunctionComponent<Props> = ({ Component, pageProps }) => {
     return (
         <StylesProvider injectFirst>
-            <Component {...pageProps} />
+            <MenuContextProvider>
+                <Component {...pageProps} />
+            </MenuContextProvider>
         </StylesProvider>
     )
 }
--- src/partials/Home/Footer.tsx
+++ src/partials/Home/Footer.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { FooterStyled, ContactIcon, Contact, FooterWrapper } from './styled'
 import instagramIcon from '../../assets/images/iconInsta.png?resize&min=25&max=75&ts-asset'
 import facebookIcon from '../../assets/images/iconFacebook.png?resize&min=25&max=75&ts-asset'
 import gmailIcon from '../../assets/images/iconGmail.png?resize&min=25&max=75&ts-asset'
 import driveIcon from '../../assets/images/iconDrive.png?resize&min=25&max=75&ts-asset'
+import { MenuContext } from '../../contexts/menuContext'
 
 const SOCIAL_MEDIA = '@innovatemeetup'
 const GOOGLE_DRIVE = 'bit.ly/innovatemeetup'
@@ -16,33 +17,40 @@ const links = {
     googleDrive: 'http://bit.ly/innovatemeetup',
 }
 
-const Footer: React.FunctionComponent = () => (
-    <FooterStyled>
-        <FooterWrapper>
-            <Contact href={links.instagram}>
-                <ContactIcon
-                    src={instagramIcon}
-                    srcSet={instagramIcon.srcSet}
-                />
-                {SOCIAL_MEDIA}
-            </Contact>
+const Footer: React.FunctionComponent = () => {
+    const { isOpen, setIsOpen, persistentMenu } = useContext(MenuContext)
 
-            <Contact href={links.facebook}>
-                <ContactIcon src={facebookIcon} srcSet={facebookIcon.srcSet} />
-                {SOCIAL_MEDIA}
-            </Contact>
+    return (
+        <FooterStyled menuIsOpen={isOpen} persistentMenu={persistentMenu}>
+            <FooterWrapper>
+                <Contact href={links.instagram}>
+                    <ContactIcon
+                        src={instagramIcon}
+                        srcSet={instagramIcon.srcSet}
+                    />
+                    {SOCIAL_MEDIA}
+                </Contact>
 
-            <Contact href={links.googleDrive}>
-                <ContactIcon src={driveIcon} srcSet={driveIcon.srcSet} />{' '}
-                {GOOGLE_DRIVE}
-            </Contact>
+                <Contact href={links.facebook}>
+                    <ContactIcon
+                        src={facebookIcon}
+                        srcSet={facebookIcon.srcSet}
+                    />
+                    {SOCIAL_MEDIA}
+                </Contact>
 
-            <Contact href={links.gmail}>
-                <ContactIcon src={gmailIcon} srcSet={gmailIcon.srcSet} />
-                {EMAIL}
-            </Contact>
-        </FooterWrapper>
-    </FooterStyled>
-)
+                <Contact href={links.googleDrive}>
+                    <ContactIcon src={driveIcon} srcSet={driveIcon.srcSet} />{' '}
+                    {GOOGLE_DRIVE}
+                </Contact>
+
+                <Contact href={links.gmail}>
+                    <ContactIcon src={gmailIcon} srcSet={gmailIcon.srcSet} />
+                    {EMAIL}
+                </Contact>
+            </FooterWrapper>
+        </FooterStyled>
+    )
+}
 
 export default Footer

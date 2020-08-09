34e9abe0b5c863fb6ec20ad5479bb4fe71a0fb92
Priscilla Lopes <cillaeslopes@gmail.com>
2020-08-09 15:49:06 -0300

    [#26] rename innovate theme to theme

---
+++ src/components/Layout/theme.ts
@@ -0,0 +1,25 @@
+export const pallete = {
+    yellow: '#ffde59',
+    pink: '#ff65c4',
+    blue: '#5ce1e6',
+    green: '#82da56',
+    purple: '#76079a',
+    black: '#212121',
+    white: '#fafafa',
+    whiteTransparency: '#ffffffbf',
+}
+
+export const typography = {
+    logo: {
+        font: 'Crop Types',
+        weight: '200',
+    },
+    title: {
+        font: 'Montserrat',
+        weight: '700',
+    },
+    text: {
+        font: 'Montserrat',
+        weight: '300',
+    },
+}

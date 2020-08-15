d4c67518c2f9be0a2746cdbce54c92a48d4b8f7c
Priscilla Lopes <cillaeslopes@gmail.com>
2020-08-15 17:01:12 -0300

    [#15] accessibility adjustments

--- src/partials/FaleConosco/SocialMedia.tsx
+++ src/partials/FaleConosco/SocialMedia.tsx
@@ -10,15 +10,24 @@ import facebookIcon from '../../assets/images/facebook.png?resize&min=50&max=150
 
 const SocialMedia: React.FunctionComponent = () => (
     <SocialMediaContainer>
-        <SocialMediaButton href="https://www.instagram.com/innovatemeetup/">
+        <SocialMediaButton
+            href="https://www.instagram.com/innovatemeetup/"
+            aria-label="instagram"
+        >
             <SocialMediaIcon src={instagramIcon} />
         </SocialMediaButton>
 
-        <SocialMediaButton href="https://join.slack.com/t/innovatebh/shared_invite/zt-gxocccq8-WsjgP55HUrZ1s1F_SBjDHw">
+        <SocialMediaButton
+            href="https://join.slack.com/t/innovatebh/shared_invite/zt-gxocccq8-WsjgP55HUrZ1s1F_SBjDHw"
+            aria-label="slack"
+        >
             <SocialMediaIcon src={slackIcon} />
         </SocialMediaButton>
 
-        <SocialMediaButton href="https://www.facebook.com/innovate.meetup">
+        <SocialMediaButton
+            href="https://www.facebook.com/innovate.meetup"
+            aria-label="facebook"
+        >
             <SocialMediaIcon src={facebookIcon} />
         </SocialMediaButton>
     </SocialMediaContainer>
